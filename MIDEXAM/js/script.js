ceda18f3dc55531a215b8b537079707f26601057
oogii0713 <oogii0713@users.noreply.github.com>
2020-04-06 12:39:35 -0500

    Updated

--- MIDEXAM/js/script.js
+++ MIDEXAM/js/script.js
@@ -1,3 +1,4 @@
+"use strict"
 $(document).ready(function() {
     clockUpdate();
     setInterval(clockUpdate, 1000);
@@ -20,21 +21,44 @@ $(document).ready(function() {
       </tr>`);
     }); 
   });
-  
+
   
   function clockUpdate() {
-    const date = new Date();
-    function addZero(x) {
-      if (x < 10) {
-        return x = '0' + x;
-      } else {
-        return x;
-      }
-    }
+      const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = monthNames[date.getMonth()];
+      const day = date.getDate();
+      const dayName = days[date.getDay()];
+      const hours = timeFormat(date.getHours());
+      const min = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+      const sec = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+      const period = defPeriod(date.getHours());
+      
 
-    const h = addZero(date.getHours());
-    const m = addZero(date.getMinutes());
-    const s = addZero(date.getSeconds());
-  
-    $('#clock').text(h + ':' + m + ':' + s)
-  }
+
+      function checkTime(hour) {
+        if( hour < 10 ) 
+          hour = '0' + hour;
+        return hour;
+      }
+
+      function timeFormat(hour) {
+        if ( hour > 12 ) 
+          hour = hour - 12;
+        return checkTime(hour);
+     }
+
+     function defPeriod(hour) {
+      if ( hour > 12 ) {
+        hour = hour - 12;
+        if (hour == 12)
+          return 'AM';
+        return 'PM';
+      }
+      return 'AM';
+     }
+
+      $('#clock').text(`${dayName}, ${day} ${month} ${year} - ${hours}:${min}:${sec} ${period}`);
+    }
