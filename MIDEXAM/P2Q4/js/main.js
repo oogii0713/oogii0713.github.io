9ac856b7320455f269e8901c668c0fbb8147fbad
oogii0713 <oogii0713@users.noreply.github.com>
2020-04-06 21:36:09 -0500

    Updated

--- MIDEXAM/P2Q4/js/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  Array.prototype.printTheName = function (name) {
-    console.log(name);
-  };
-
-  let array = [];
-  array.printTheName("Anna H. Smith");
-  array.printTheName("Bob J. Harrison");
-})();
